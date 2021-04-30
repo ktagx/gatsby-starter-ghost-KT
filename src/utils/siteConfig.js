1cc13fa15e3b50f0ea4ee849dd847cbe1be6415d
ktagx <83347072+ktagx@users.noreply.github.com>
2021-04-30 11:40:57 -0500

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -3,13 +3,13 @@ module.exports = {
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Spain Startup News`, // This allows an alternative site title for meta data for pages.
+    siteTitleMeta: `Spain`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Breaking news on top tech startups in Spain`, // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Spain Startup News`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `Spain`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
